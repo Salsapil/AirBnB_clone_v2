59fd057959abc5cc7e1ae1779e30427c2982c2a1
Salsabil <salsabeelahmed323@gmail.com>
2024-05-04 20:33:59 +0300

    task1-test2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,16 +6,16 @@ import os
 
 
 def do_pack():
-  try:
-    now = datetime.now()
-    filename = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
-    versions_dir = "versions"
-    os.makedirs(versions_dir, exist_ok=True)
-    archive_path = os.path.join(versions_dir, filename)
-    local(f"tar -cvzf {archive_path} web_static")
+    try:
+        now = datetime.now()
+        filename = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+        versions_dir = "versions"
+        os.makedirs(versions_dir, exist_ok=True)
+        archive_path = os.path.join(versions_dir, filename)
+        local(f"tar -cvzf {archive_path} web_static")
 
-    print(f"web_static packed: {archive_path}")
-    return archive_path
+        print(f"web_static packed: {archive_path}")
+        return archive_path
 
-  except:
-    return None
+    except:
+        return None
