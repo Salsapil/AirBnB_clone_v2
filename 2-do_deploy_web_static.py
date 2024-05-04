1239662d7762f92976ffed9de376184a9e2f4257
Salsabil <salsabeelahmed323@gmail.com>
2024-05-04 19:12:14 +0300

    test_task3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """distributes an archive to the web servers"""
 from os.path import exists
-from fabric.api import put, env, sudo
-
-
+from fabric.api import put, env, run
 env.hosts = ['54.236.41.47', '100.25.131.39']
 
 
@@ -13,25 +11,17 @@ def do_deploy(archive_path):
         return False
     try:
         file_name = archive_path.split("/")[-1]
-        dir_name = file_name.split(".")[0]
-        tmp = f"/tmp/{file_name}"
-        ex_dir = f"/data/web_static/releases/{dir_name}/"
-    
-        put(archive_path, tmp, use_sudo=True)
-        sudo(f"mkdir -p {ex_dir}")
-        sudo(f"tar -xzf {tmp} -C {ex_dir}")
-        sudo(f"rm {tmp}")
-    
-        dir1 = f"/data/web_static/releases/{dir_name}/web_static/*"
-        dir2 = f"/data/web_static/releases/{dir_name}/"
-
-        sudo("mv {} {}".format(dir1, dir2))
-        sudo(f"rm -rf /data/web_static/releases/{dir_name}/web_static")
-        l1 = f"/data/web_static/releases/{dir_name}/"
-        lc = "/data/web_static/current"
-
-        sudo("rm -rf {}".format(lc))
-        sudo("ln -s {} {}".format(l1, lc))
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,3 +1,48 @@
 #!/usr/bin/python3
 """creates and distributes an archive to web servers"""
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['54.236.41.47', '100.25.131.39']
 
+
+def do_pack():
+    """fab function"""
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local(f'tar -cvzf versions/{archive} web_static')
+    if create is not None:
+        return archive
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
