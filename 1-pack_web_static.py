0e95d53fcbbf00b666ce1e261050ab2692a1cf13
Salsabil <salsabeelahmed323@gmail.com>
2024-05-04 20:37:56 +0300

    task1-test2.2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """web static pack module"""
 from datetime import datetime
-from fabric.api import local
-import os
+from fabric.api import *
 
 
 def do_pack():
-    try:
-        now = datetime.now()
-        filename = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
-        versions_dir = "versions"
-        os.makedirs(versions_dir, exist_ok=True)
-        archive_path = os.path.join(versions_dir, filename)
-        local(f"tar -cvzf {archive_path} web_static")
+    """fab function"""
 
-        print(f"web_static packed: {archive_path}")
-        return archive_path
-
-    except Exception:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local(f'tar -cvzf versions/{archive} web_static')
+    print(f"web_static packed: {archive}")
+    if create is not None:
+        return archive
+    else:
         return None
+
