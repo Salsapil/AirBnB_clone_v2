644495ba72fe406996b4d8128fb2ec66b12ffce6
Salsabil <salsabeelahmed323@gmail.com>
2024-05-04 19:02:34 +0300

    test1.4

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,34 +7,31 @@ from fabric.api import put, env, sudo
 env.hosts = ['54.236.41.47', '100.25.131.39']
 
 
-def do_pack():
-    """fab function"""
-
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local(f'tar -cvzf versions/{archive} web_static')
-    if create is not None:
-        return archive
-    else:
-        return None
-
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        sudo('mkdir -p {}{}/'.format(path, no_ext))
-        sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        sudo('rm /tmp/{}'.format(file_n))
-        sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        sudo('rm -rf {}{}/web_static'.format(path, no_ext))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        file_name = archive_path.split("/")[-1]
+        dir_name = file_name.split(".")[0]
+        tmp = f"/tmp/{file_name}"
+        ex_dir = f"/data/web_static/releases/{dir_name}/"
+    
+        put(archive_path, tmp, use_sudo=True)
+        sudo(f"mkdir -p {ex_dir}")
+        sudo(f"tar -xzf {tmp} -C {ex_dir}")
+        sudo(f"rm {tmp}")
+    
+        dir1 = f"/data/web_static/releases/{dir_name}/web_static/*"
+        dir2 = f"/data/web_static/releases/{dir_name}/"
+
+        sudo("mv {} {}".format(dir1, dir2))
+        sudo(f"rm -rf /data/web_static/releases/{dir_name}/web_static")
+        l1 = f"/data/web_static/releases/{dir_name}/"
+        lc = "/data/web_static/current"
+
+        sudo("rm -rf {}".format(lc))
+        sudo("ln -s {} {}".format(l1, lc))
         return True
     except:
         return False
