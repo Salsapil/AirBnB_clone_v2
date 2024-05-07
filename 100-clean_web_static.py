8406f0ffdc4a0408752b5e623f46e43dd11bdd3e
Salsabil <salsabeelahmed323@gmail.com>
2024-05-07 12:16:59 +0300

    airbnb_test1

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""deletes out-of-date archives"""
+import os
+from fabric.api import *
+env.hosts = ['54.236.41.47', '100.25.131.39']
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives"""
+    if int(number) == 0:
+        number = 1
+    else:
+        int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
