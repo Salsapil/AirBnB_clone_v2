a634d853548a1f65961ae12e68b0370873474d60
Salsabil <salsabeelahmed323@gmail.com>
2024-05-07 12:36:41 +0300

    airbnb_test3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
 """deletes out-of-date archives"""
-from fabric.api import env, run, local
+import os
+from fabric.api import *
 env.hosts = ['54.236.41.47', '100.25.131.39']
 
 
 def do_clean(number=0):
     """Deletes out-of-date archives"""
-    if number < 0:
-        raise ValueError("number must be non-negative")
+    number = 1 if int(number) == 0 else int(number)
 
-    # Get a list of archive files in both locations
-    local_archives = local("ls -tr versions/*.tgz | \
-        awk '{print $NF}'", capture=True).splitlines()
-    remote_archives = run("ls -tr /data/web_static/releases/*.tgz | \
-        awk '{print $NF}'", hosts=env.hosts).splitlines()
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    # Keep the specified number of most recent archives in each location
-    local_to_keep = local_archives[-number:]
-    remote_to_keep = remote_archives[-number:]
-
-    # Delete unnecessary local archives
-    for archive in local_archives:
-        if archive not in local_to_keep:
-            run(f"rm versions/{archive}", hosts=env.hosts)
-
-    # Delete unnecessary remote archives
-    for archive in remote_archives:
-        if archive not in remote_to_keep:
-            run(f"rm /data/web_static/releases/{archive}", hosts=env.hosts)
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
