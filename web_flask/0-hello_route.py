49781c81ceccd01dbc606b95c7df712a989c05b3
Salsabil <salsabeelahmed323@gmail.com>
2024-05-17 14:12:53 +0300

    AirBnB_clone_v2_web_frame- pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 from flask import Flask
-app  = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
@@ -9,6 +9,6 @@ def home():
     """return string"""
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
