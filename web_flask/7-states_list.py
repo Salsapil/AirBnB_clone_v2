1afd5936a55fd37a895c905b5a01dd37aaa06d16
Salsabil <salsabeelahmed323@gmail.com>
2024-05-19 23:24:29 +0300

    AirBnB_clone_v2_web_frame- Task8.1

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,12 +6,18 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def close_storage():
-    """method to handle @app.teardown_appcontext"""
+def close_storage(exception):
+    """method to handle"""
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-    state = storage.all(State)
-    render_template('7-states_list.html', state = state)
+    state = storage.all('State')
+    return render_template('7-states_list.html', state=state)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
